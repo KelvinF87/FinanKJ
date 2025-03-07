dad726a3f7c11c0f5b1b4f28a8edf427201d6afb
KelvinFamilia <kelvinjosefamilia@gmail.com>
2025-03-07 17:39:23 +0000

    cambio de lugar de context CheckUseStatus

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { Ajustes } from "./pages/Ajustes";
 import Profile from "./pages/Profile";
 import { useContext } from "react";
 import { AuthForm } from "./components/Login";
-import CheckUserStatus from "./components/CheckUserStatus";
+import CheckUserStatus from "./contexts/CheckUserStatus";
 
 function App() {
   const API_URL = import.meta.env.VITE_API_URI;
